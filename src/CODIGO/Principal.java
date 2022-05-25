d3ddbeb0235c0d080bb5446b280713ccc57ccbb9
carlosangulom <49261785+carlosangulom@users.noreply.github.com>
2022-05-24 23:20:45 -0600

    0.0.1

---
+++ src/CODIGO/Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CODIGO;
+
+import INTERFAZ.*;
+/**
+ *
+ * @author Carlos
+ */
+public class Principal {
+    public static void main(String[] args) {
+        Interfaz main = new Interfaz();
+        main.setVisible(true);
+    }
+}
